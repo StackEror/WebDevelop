6b6630be04400b7c641b134b98b449b64e2d40b5
Lucian <lucian.enachi@smartgov.md>
2025-11-30 19:17:54 +0200

    added entityFramework

---
+++ WebDevelopment.Infrastructure/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebDevelopment.Domain.Entities;
+
+namespace WebDevelopment.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Country> Countries { get; set; }
+}
