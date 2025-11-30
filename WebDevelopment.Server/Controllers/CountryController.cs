020393b0a9bed64e911cbaafbe9a706ddc3bf3ba
Lucian <lucian.enachi@smartgov.md>
2025-11-30 23:27:56 +0200

    added controller, front changes

---
+++ WebDevelopment.Server/Controllers/CountryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebDevelopment.Domain.Entities;
+
+namespace WebDevelopment.Server.Controllers;
+
+[ApiController]
+[Route("api/country")]
+public class CountryController : ControllerBase
+{
+    [HttpPost("add")]
+    public async Task<IActionResult> AddCountry(Country country)
+    {
+        return Ok();
+    }
+}
