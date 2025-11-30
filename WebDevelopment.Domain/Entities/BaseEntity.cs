0da579cc7002fd505a3c1a7e112bec4ac05ba538
Lucian <lucian.enachi@smartgov.md>
2025-11-30 18:57:49 +0200

    added BaseEntity

---
+++ WebDevelopment.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace WebDevelopment.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ?Description { get; set; }
+    }
+}
