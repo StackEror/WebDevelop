3c59b7957842d7f2e6debb87bd0dafee92c4067c
Lucian <lucian.enachi@smartgov.md>
2025-12-04 02:16:50 +0200

    added js func for counter and validation + C# oninput event

---
+++ WebDevelopment.Client/wwwroot/js/htmlEditor.js
@@ -0,0 +1,31 @@
+﻿window.editorCounterValidator = function (textLimit, dotNetRef) {
+    const editor = document.getElementById("htmlEditor");
+    const limit = textLimit;
+
+    if (!editor) return;
+
+    editor.addEventListener("input", () => {
+
+        let text = editor.value || "";
+        console.log("text", text);
+        let leftLen = limit - text.length;
+        console.log("len", leftLen);
+        dotNetRef.invokeMethodAsync("ChangeLimitError", text.length, leftLen);
+    });
+}
+
+
+//window.initHtmlEditorCounter = function (dotNetRef) {
+//    const editor = document.getElementById("htmlEditor");
+//    const limit = 12000;
+
+//    if (!editor) return;
+
+//    editor.addEventListener("input", () => {
+
+//        let text = editor.innerText || "";
+//        let len = limit - text.length;
+//        console.log("len", len);
+//        dotNetRef.invokeMethodAsync("ChangeLimitError", editor.value.length);
+//    });
+//}
