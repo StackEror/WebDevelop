1cf175bd94371664d94f51bb4ab47112a0a3b79e
Lucian <lucian.enachi@smartgov.md>
2025-12-06 00:04:23 +0200

    Added automapper

---
+++ WebDevelopment.Application/Mapper/CountryMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebDevelopment.Domain.Entities;
+using WebDevelopment.Shared.DTO;
+
+namespace WebDevelopment.Application.Mapper;
+
+public class CountryMappingProfile : Profile
+{
+    public CountryMappingProfile()
+    {
+        CreateMap<CountryDto, Country>()
+            .ReverseMap();
+    }
+}
