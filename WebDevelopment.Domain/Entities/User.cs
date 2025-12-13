16a151a4c566a67be091acdf26984ef85e8699ac
Lucian <lucian.enachi@smartgov.md>
2025-12-13 16:50:39 +0200

    added modal, changes list page, added user entity

---
+++ WebDevelopment.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WebDevelopment.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Role { get; set; }
+        public string UserName { get; set; }
+        public string? Email { get; set; }
+        public int Age { get; set; }
+        public string? Nationality { get; set; }
+    }
+}
