b7122e9b21edaa2b1507eb9a575b560c2d6b9cdc
Lucian <lucian.enachi@smartgov.md>
2025-12-20 19:11:30 +0200

    added file test page

---
+++ WebDevelopment.Shared/DTO/FileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebDevelopment.Shared.DTO;
+
+public class FileDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
+    public byte[]? Content { get; set; }
+}
