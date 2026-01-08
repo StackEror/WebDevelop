f6210c679649bae9f920682732a5f7c1f66ac253
Lucian <lucian.enachi@smartgov.md>
2026-01-08 16:00:22 +0200

    added Roles class

---
+++ WebDevelopment.Shared/Roles/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace WebDevelopment.Shared.Roles;
+
+public static class Roles
+{
+    public const string Admin = "Admin";
+    public const string Client = "Client";
+
+    public static string Merge(params string[] roles) => string.Join(", ", roles);
+}
